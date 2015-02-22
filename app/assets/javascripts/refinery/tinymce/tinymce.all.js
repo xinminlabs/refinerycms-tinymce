23f1e353fea7137c17bdbe541d25f769c2e10df6
Richard Huang <flyerhzm@gmail.com>
2015-02-22 21:23:28 +0800

    customize configs

--- app/assets/javascripts/refinery/tinymce/tinymce.all.js
+++ app/assets/javascripts/refinery/tinymce/tinymce.all.js
@@ -23,9 +23,13 @@ $(function() {
 
     options.convert_urls = false;
     options.selector = ".wysiwyg-editor";
-    options.plugins = "advlist code table lists charmap autolink hr anchor pagebreak fullscreen textcolor refineryfilemanager"
-    options.toolbar1 = "undo redo |  bullist numlist outdent indent | image media | refineryfilemanager";
-    options.toolbar2 = "styleselect | bold italic underline | fontselect forecolor backcolor";
+    options.plugins = "advlist code table lists charmap autolink link hr anchor pagebreak fullscreen textcolor refineryfilemanager image media";
+    options.menubar = false;
+    //options.toolbar1 = "undo redo |  bullist numlist outdent indent | alignleft aligncenter alignright alignjustify | image media | refineryfilemanager";
+    //options.toolbar2 = "styleselect | bold italic underline | fontselect forecolor backcolor | table";
+    options.toolbar1 = "bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | formatselect fontselect fontsizeselect",
+    options.toolbar2 = "bullist numlist | outdent indent blockquote | undo redo | link unlink  | refineryfilemanager media | forecolor backcolor | hr removeformat | subscript superscript | table",
+
     options.image_advtab = true;
     options.external_filemanager_path  = backend_path();
 
